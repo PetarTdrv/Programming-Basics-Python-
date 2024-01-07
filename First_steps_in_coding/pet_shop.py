f45845092313c2746d425eda4d2f32ef150c490b
Petar Todorov <154418284+PetarTdrv@users.noreply.github.com>
2024-01-07 23:48:54 +0200

    Create pet_shop.py

---
+++ First_steps_in_coding/pet_shop.py
@@ -0,0 +1,8 @@
+number_dog_food = int(input())
+number_cat_food = int(input())
+
+price_dog = number_dog_food * 2.50
+price_cat = number_cat_food * 4
+
+full_price = price_dog + price_cat
+print(f"{full_price} lv.")
