6294462b41dfa9740195ddda7259fa40bdc8e51a
Petar Todorov <154418284+PetarTdrv@users.noreply.github.com>
2024-01-07 23:47:54 +0200

    Create making_projects.py

---
+++ First_steps_in_coding/making_projects.py
@@ -0,0 +1,7 @@
+name_of_architect = input()
+number_of_projects = int(input())
+
+time = number_of_projects * 3
+
+print(f"The architect {name_of_architect} \
+will need {time} hours to complete {number_of_projects} project/s.")
