10018c35f8993eb654d6bdc33f9b59fe7bdfda4a
Petar Todorov <154418284+PetarTdrv@users.noreply.github.com>
2024-01-08 10:58:45 +0200

    Create basketball_equipment.py

---
+++ First_steps_in_coding_exercise/basketball_equipment.py
@@ -0,0 +1,9 @@
+annual_tax = int(input())
+
+basketball_sneakers = annual_tax * 0.60
+basketball_kit = basketball_sneakers * 0.80
+basketball_ball = basketball_kit / 4
+basketball_accesoaries = basketball_ball / 5
+
+full_price_for_equipment = annual_tax + basketball_sneakers + basketball_kit + basketball_ball + basketball_accesoaries
+print(full_price_for_equipment)
